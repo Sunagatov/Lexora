ca09c5bd64de1b1a80aa729fbb38d11b393ce9ac
zufar-sunagatov <zufar.sunagatov@gmail.com>
2026-04-08 00:05:59 +0100

    improved

--- frontend/src/components/study/TopicSidebar.tsx
+++ frontend/src/components/study/TopicSidebar.tsx
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 import {createPortal} from 'react-dom'
-import type {Topic, Word} from '../../lib/api'
+import type {Topic} from '../../lib/api'
 
 type Props = {
   topics: Topic[]
