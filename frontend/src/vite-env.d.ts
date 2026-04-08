6b3017a43d88098114e72cb229962d567e382105
zufar-sunagatov <zufar.sunagatov@gmail.com>
2026-04-08 20:17:47 +0100

    refactoring

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string
+  readonly VITE_PAGE_SIZES: string
+  readonly VITE_DEFAULT_PAGE_SIZE: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
